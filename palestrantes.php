ca414c4b16ac602a47c823938ca7ee8ab805013c
Arthur <guy.art123.ad@gmail.com>
2025-02-25 11:16:08 -0300

    adicionando pag palestrantes

---
+++ palestrantes.php
@@ -0,0 +1,313 @@
+<?php
+require_once('html_header.php');
+require_once('header.php');
+?>
+
+<!--==========================
+            Programação Geral
+        ============================-->
+
+<section id="schedule" class="section-with-bg">
+    <div class="container wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+        <div class="section-header">
+            <h2>Palestrantes</h2>
+        </div>
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Adriana Gomes Nogueira Ferreira</h5>
+                            <p class="card-text">
+                                UFMA 
+                                <br> 
+                                Doutora em Enfermagem - UFC.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Alan Bezerra Ribeiro</h5>
+                            <p class="card-text">
+                                Doutorado em Química Orgânica - Universidade Federal de São Carlos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Antônio Jardson dos Santos Lima</h5>
+                            <p class="card-text">
+                                MBA em Gestão de Pessoas 
+                                <br> 
+                                Coordenador de Propriedade Intelectual junto à Coordenação de Prospecção e Redação de Patentes-CPRP - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Caroline Heitmann Mares Azevedo Ribeiro</h5>
+                            <p class="card-text"> 
+                                Doutorado em Farmácia - USP
+                                <br>
+                                Docente - UFPA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Caroline Martins de Jesus</h5>
+                            <p class="card-text">
+                                Mestranda em Saúde e Tecnologia - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Cleber Augusto Pereira</h5>
+                            <p class="card-text">
+                                Doutor em Engenharia Electrotécnica e de Computadores - Universidade de Trás-os-Montes e Alto Douro - Portugal.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Cristina Maria Assis Lopes Tavares da Mata Hermida Quintella</h5>
+                            <p class="card-text">
+                                Doutorado em School of Molecular Sciences pela University of Sussex, Inglaterra 
+                                <br> 
+                                Docente - UFBA.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Elizabeth Teixera</h5>
+                            <p class="card-text">
+                                Doutora em Ciências: Desenvolvimento Sócio Ambiental 
+                                <br> 
+                                Professora Visitante - UFPA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Francisco Dimitre Rodrigo Pereira Santos</h5>
+                            <p class="card-text">
+                                IESMA/UNISULMA e UNITINS Doutorando em Engenharia Biomédica - Universidade Anhembi Morumbi.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Geysa Santos Góis Lopes</h5>
+                            <p class="card-text">
+                                Universidade do Porto - Portugal
+                                <br> 
+                                Doutoranda do Programa Doutoral em Ciências de Enfermagem.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">José Aquino Junior</h5>
+                            <p class="card-text">
+                                Doutor em Geografia - Docente - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Kélcio Pereira Miranda</h5>
+                            <p class="card-text"> 
+                                Mestrando em Saúde e Tecnologia - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Lívio Martins Costa Júnior</h5>
+                            <p class="card-text">
+                                Doutorado em Parasitologia - UFMG 
+                                <br> 
+                                Docente - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Louriane Nunes Gomes</h5>
+                            <p class="card-text">
+                                Mestranda em Saúde e Tecnologia - UFMA.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Luis Gustavo Gasparini Kiatake</h5>
+                            <p class="card-text">
+                                Mestrado em Engenharia Elétrica - USP 
+                                <br> 
+                                Presidente da Sociedade Brasileira de Informática em Saúde.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Márcia Cristina Gonçalves Maciel</h5>
+                            <p class="card-text">
+                                Doutora em Biotecnologia
+                                <br> 
+                                Docente - UFMA e UnB.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Márcio Flávio Moura de Araújo</h5>
+                            <p class="card-text">
+                                Doutorado em Enfermagem - UFC
+                                <br> 
+                                Pesquisador em Saúde Pública da Fundação Oswaldo Cruz.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Rafaela Cristina Araújo Gomes</h5>
+                            <p class="card-text">
+                                Mestrado em Saúde e Tecnologia - UFMA
+                                <br> 
+                                Doutoranda em Enfermagem e Biociências - UNIRIO.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Romel Jonathan Velasco Yanez</h5>
+                            <p class="card-text">
+                                Mestrando em Enfermagem - UFC
+                                <br> 
+                                Mestrando em Enfermagem com foco na Gestão da Saúde - Universidad Internacional Iberoamericana de México.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Ricardo Alexandre Arcêncio</h5>
+                            <p class="card-text">
+                                Doutor em Ciências
+                                <br> 
+                                Docente - EERP/USP.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card profile-card-2">
+                        <div class="card-body pt-5">
+                            <img src="img/speakers/pet.jpg" alt="profile-image" class="profile">
+                            <h5 class="card-title">Thiago Moura Araújo</h5>
+                            <p class="card-text">
+                                Doutor em Enfermagem - UFC
+                                <br> 
+                                Docente da UNILAB.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>     
+
+        </div>
+    </div>
+    </div>
+
+    </div>
+
+
+</section>
+
+<?php
+require_once('footer.php');
+require_once('html_footer.php');
+?>

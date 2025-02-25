cd07660efbba8587f3dd8d30cf79f45320f5444a
Arthur <guy.art123.ad@gmail.com>
2025-02-25 11:54:25 -0300

    alterando local e comissao

--- comissao.php
+++ comissao.php
@@ -15,30 +15,43 @@
   require_once('header.php');
 ?>
   <div class="section-header">
-  <h1 id="centered">COMISSÃO</h1>
-            </div>
+    <h1 id="centered">COMISSÃO</h1>
+  </div>
 <div class="content">
 
   <h3 class="centered">Comissão Organizadora</h3>
   <ul class="centered">
     <li>Adriana Gomes Nogueira Ferreira - Docente PPGST/UFMA</li>
-    <li>Janaina Miranda Bezerra - Docente Coordenação Enfermagem/UFMA</li>
-    <li>Ismália Cassandra Costa Maia Dias - Docente PPGST/UFMA</li>
     <li>Queli Cristina Fidelis - Docente PPGST/UFMA</li>
-    <li>Floriacy Stabnow Santos - Docente PPGST/UFMA</li>
+    <li>Aramys Silva dos Reis - Docente PPGST/UFMA</li>
+    <li>Ismália Cassandra Costa Maia Dias - Docente PPGST/UFMA</li>
     <li>Patrícia Neyva da Costa Pinheiro - Docente PPGENF/UFC</li>
-    <li>Edna Johana Mondragón Sánchez - Docente FCS - Universidade do Quindio Colômbia</li>
-    <li>Antonia Mayara Brilhante de Sousa - Discente PPGST/UFMA</li>
-    <li>Renata Pereira Almeida - Discente PPGST/UFMA</li>
+    <li>Silvia Cristina Martini - Docente PPEBM/UMC</li>
+    <li>Vanessa Denardi Antoniassi Baldissera - Docente PPGENF/UEM</li>
+    <li>Mark Henrique Santana Silva - Discente PPGST/UFMA</li>
+    <li>Jurandir Xavier de Sá Júnior - Discente PPGST/UFMA</li>
+    <li>Gabriel da Silva Martins - Discente PPGST/UFMA</li>
+    <li>Daianne Santos de Souza - Discente PPGST/UFMA</li>
   </ul>
 
   <h3 class="centered">Comissão Científica</h3>
   <ul class="centered">
-    <li>Ana Lucia Fernandes Pereira - Docente PPGST/UFMA</li>
-    <li>Virginia Kelly Gonçalves Abreu - Docente PPGST/UFMA</li>
-    <li>Marcia Cristina Gonçalves Maciel - Docente PPGST/UFMA</li>
-    <li>Lívia Maia Pascoal - Docente PPGST/UFMA</li>
-    <li>Marcelino Santos Neto - Docente PPGST/UFMA</li>
+    <li>Ana Lucia Fernandes Pereira - PPGST/UFMA</li>
+    <li>Ana Cristina Pereira de Jesus Costa - PPGST/UFMA</li>
+    <li>Virginia Kelly Gonçalves Abreu - PPGST/UFMA</li>
+    <li>Marcia Cristina Gonçalves Maciel - PPGST/UFMA</li>
+    <li>Lívia Maia Pascoal - PPGST/UFMA</li>
+    <li>Marcelino Santos Neto - PPGST/UFMA</li>
+    <li>Floriacy Stabnow Santos - PPGST/UFMA</li>
+    <li>Leonardo Hunaldo dos Santos - PPGST/UFMA</li>
+    <li>Queli Cristina Fidelis - PPGST/UFMA</li>
+    <li>Maria Aparecida Alves de Oliveira Serra - PPGST/UFMA</li>
+    <li>Janaina Miranda Bezerra - RENASF/UFMA</li>
+    <li>Carolina Heitmann Mares Azevedo Ribeiro - UFPA</li>
+    <li>Geovany Amorim Gomes - UVA</li>
+    <li>Vanessa Denardi Antoniassi Baldissera - PPGENF/UEL</li>
+    <li>Francisco Stélio de Sousa - PPGST/UEPB</li>
+    <li>Silvia Cristina Martini - PPGEBM/UMC</li>
     <li>Paula dos Santos Brito - Discente PPGST/UFMA</li>
     <li>Aldo Lopes da Costa Junior - Discente PPGST/UFMA</li>
     <li>Daniel Coutinho dos Santos - Discente PPGST/UFMA</li>

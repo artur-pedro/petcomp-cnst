34b477d9353e3570b81b047a0bde7983453f39d6
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2025-02-25 11:47:31 -0300

    Mudando coisas de novo

--- inscricoes.php
+++ inscricoes.php
@@ -20,7 +20,7 @@
 
 <div class="content">
 
-  <p class="centered">Para fazer sua inscrição no evento, <a href="#">clique aqui</a>.</p>
+  <p class="centered">Para fazer sua inscrição no evento, <a href="https://doity.com.br/ii-congresso-nacional-de-saude-e-tecnologia-e-4-cafe-com-ciencia-ppgst"  target="_blank">clique aqui</a>.</p>
 
   <h3 class="centered">PERÍODO DE INSCRIÇÃO</h3>
   <ul class="centered">
